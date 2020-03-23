40a3cd7c5bc7f89de0cc3a53ca0f010097019141
Ryan Goderis <rgoderis789@gmail.com>
2020-03-23 15:44:41 -0400

    set up routes created controllers dir with controller file, in client package.json set proxy of localhost:3001 and installed react-router-dom, installed body-parser and cookie-parser, set middleware for both body and cookie parser.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(routes)
 
 if (process.env.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, '../build')));
