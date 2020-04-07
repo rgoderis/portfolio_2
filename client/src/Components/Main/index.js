9c2c159d9c650aae4c056f58b840b4566c0aecb8
Ryan Goderis <rgoderis789@gmail.com>
2020-04-07 10:07:41 -0400

    imported headshot image in Main and called headshot in img tag.  Added functionality to email icon to generate an email with prefilled subject.

--- client/src/Components/Main/index.js
+++ client/src/Components/Main/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./style.css"
+import headshot from "../../images/LowRezHeadshot.jpg"
 
 function Main(){
+    let clickListener = ()=>{
+        window.open('mailto:rgoderis789@gmail.com?subject=Reaching Out');
+    }
     return(
         <div className="jumbotron container mt-5 background-primary">
             <h1 className="text-center text-white">Ryan Goderis</h1>
             <figure className="text-center container">
-                <p>image</p>
+                <img src={headshot} alt="profile image" className="img-thumbnail text-center p-0 m-0 w-25"/>
             </figure>
             <main className="container">
                 <h2 className="text-center mt-5 mb-5 text-white">Front End Developer and Caffeine Addict</h2>
@@ -16,7 +20,7 @@ function Main(){
                         A quick learner that is enthusiastic in tackling new challenges with a passion for growing and developing as a programer.</p>
                 </section>
                 <section className="text-center">
-                    <a href="rgoderis789@gmail.com" className="text-info btn btn-lg"><i class="fas fa-envelope"></i></a>
+                    <a href="rgoderis789@gmail.com" className="text-info btn btn-lg" onClick={clickListener}><i class="fas fa-envelope"></i></a>
                     <a href="https://www.linkedin.com/in/ryan-goderis" className="text-info btn btn-lg"><i className="fab fa-linkedin"></i></a>
                     <a href="https://github.com/rgoderis" className="text-info btn btn-lg"><i className="fab fa-github-square"></i></a>
                 </section>
