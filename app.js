340c3e76ee27ddd90166c5a78f089f1e9937f348
Ryan Goderis <rgoderis789@gmail.com>
2020-04-08 14:19:27 -0400

    added error handeling in app.js file

--- app.js
+++ app.js
@@ -13,11 +13,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, './client/build')));
+ };
+
 app.use(routes)
 
-if (process.env.NODE_ENV === "production") {
-   app.use(express.static(path.join(__dirname, './client/build')));
-  };
+/* === Express 404 error handler === */
+app.use(function(req, res, next) {
+  var err = new Error('404 in Server.js, route Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/* Development error handler will print stacktrace */
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+          message: err.message,
+          error: err
+      });
+  });
+}
+
+/* Production error handler no stacktraces leaked to user */
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+      message: err.message,
+      error: {}
+  });
+});
 
 app.listen(PORT, function(){
     console.log(`server on port${PORT}`)
