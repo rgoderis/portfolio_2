806049afa4545df875daeabf44be931e3e05a5b7
Ryan Goderis <rgoderis789@gmail.com>
2020-04-08 14:04:46 -0400

    added res.sendFile to api/index.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const controller = require('../../controllers/controller');
 
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  });
+
 module.exports = router
