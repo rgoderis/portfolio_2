17f4a550c3b02f15abc08454c866ac5c0f2f3c33
Ryan Goderis <rgoderis789@gmail.com>
2020-04-20 13:02:21 -0400

    created page of AddContact set to /add, created function of checkContact in utils folder, set checkContact to make a fetch request to addressBook app retrieve information of user rgoderis

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 const API = {
     checkContact: function(){
-        axios.get('https://cors-anywhere.herokuapp.com/https://immense-harbor-83008.herokuapp.com/api/users/rgoderis')
+        fetch('https://cors-anywhere.herokuapp.com/immense-harbor-83008.herokuapp.com/api/users/rgoderis')
         .then(res=>console.log(res))
         .catch(err=>console.log(err))
+        // axios.get('https://cors-anywhere.herokuapp.com/https://immense-harbor-83008.herokuapp.com/api/users/rgoderis')
+        // .then(res=>console.log(res))
+        // .catch(err=>console.log(err))
     },
-    addContact: function(){
-        
-    }
 
 }
 
