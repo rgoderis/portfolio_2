89b417202e4abdc4b5a0a2ab9278fb94e3aabcae
Ryan Goderis <rgoderis789@gmail.com>
2020-04-27 12:58:39 -0400

    removed checkContact function as well as inputs for address in AddContact page.

--- client/src/Pages/AddContact.js
+++ client/src/Pages/AddContact.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import axios from 'axios'
 import Input from '../Components/Input'
-import DropDown from "../Components/DropDown"
 
 class AddContact extends React.Component{
     state = {
-        states: ["","AL","AK", "AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA", "WA","WV","WI","WY"],
         firstName: "",
         lastName: "",
         email: "",
         phoneNumber: "",
-        address: "",
-        city: "",
-        state: "",
-        zip: "",
         company: "",
         jobTitle: "",
-        contactInfo: "",
         contactAdded: false
     }
 
@@ -45,10 +38,6 @@ class AddContact extends React.Component{
                 lastName: this.state.lastName,
                 phoneNumber: this.state.phoneNumber,
                 email: this.state.email,
-                address: this.state.address,
-                city: this.state.city,
-                state: this.state.state,
-                zip: this.state.zip,
                 company: this.state.company,
                 jobTitle: this.state.jobTitle
             })
@@ -64,13 +53,6 @@ class AddContact extends React.Component{
             .catch(err=>console.log(err))
         }
     }
-    checkContact = ()=>{
-        axios.get('https://cors-anywhere.herokuapp.com/https://immense-harbor-83008.herokuapp.com/api/users/rgoderis')
-        .then(res=>{
-            this.setState({contactInfo: res.data[0]})
-        })
-        .catch(err=>console.log(err))
-    }
 
     componentDidMount(){
         this.checkContact()
@@ -81,7 +63,8 @@ class AddContact extends React.Component{
                 {this.state.contactAdded?
                 <h1 className="text-center">Contact Information Added</h1>:
                 <div>
-                <h2 className="text-center mb-5 mt-5">Enter Your Contact Information Below</h2>
+                <h2 className="text-center mb-5 mt-5">Interested in Hearing More?</h2>
+                <h3>Email me by clicking here or fill out the information below and I will respond ASAP!</h3>
                 <form>
                     <div className="form-group">
                         <div className="row">
@@ -143,46 +126,6 @@ class AddContact extends React.Component{
                                     value = {this.state.jobTitle}
                                     onChange = {this.handleInputChange}
                                     name={"jobTitle"}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label>Address</label>
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <Input
-                                    placeholder = "Address"
-                                    type= "text"
-                                    value = {this.state.address}
-                                    onChange = {this.handleInputChange}
-                                    name = "address"
-                                />
-                            </div>
-                            <div className="col-lg-3">
-                                <Input
-                                    placeholder = "City"
-                                    type= "text"
-                                    value = {this.state.city}
-                                    onChange = {this.handleInputChange}
-                                    name = "city"
-                                />
-                            </div>
-                            <div className="col-lg-1">
-                                <DropDown
-                                    array = {this.state.states}
-                                    value = {this.state.state}
-                                    onChange = {this.handleInputChange}
-                                    name={"state"}
-                                />
-                            </div>
-                            <div className="col-lg-2">
-                                <Input
-                                    placeholder = "Zip"
-                                    type= "text"
-                                    value = {this.state.zip}
-                                    onChange = {this.handleInputChange}
-                                    name = "zip"
                                 />
                             </div>
                         </div>
